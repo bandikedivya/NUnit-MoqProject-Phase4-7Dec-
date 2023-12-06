0c670e336e5ca3c80b09e30ed532a4081227af74
bandikedivya <divyacrkdv@gmail.com>
2023-12-06 18:13:27 +0530

    Add project files.

---
+++ SchoolLib/NUnitTestPrjt/TeacherTests.cs
@@ -0,0 +1,41 @@
+﻿using SchoolLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestPrjt
+{
+    [TestFixture]
+    public class TeacherTests
+    {
+        [Test]
+        public void TestTeacherName()
+        {
+            // Arrange
+            Teacher myTeacher = new Teacher();
+            myTeacher.Name = "Ravi Kumar";
+
+            // Act
+            string teacherName = myTeacher.Name;
+
+            // Assert
+            Assert.AreEqual("Ravi Kumar", teacherName);
+        }
+
+        [Test]
+        public void TestClassId()
+        {
+            // Arrange
+            Teacher myTeacher = new Teacher();
+            myTeacher.Id = 1;
+
+            // Act
+            int tecaherId = myTeacher.Id;
+
+            // Assert
+            Assert.AreEqual(1, tecaherId);
+        }
+    }
+}
